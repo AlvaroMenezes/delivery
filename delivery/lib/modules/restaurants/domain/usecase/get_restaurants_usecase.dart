b4b9fbec7c5f71932977f918a1c62b2c20bafcc8
Alvaro Menezes <alvarom@ufcspa.edu.br>
2021-01-20 18:03:56 -0300

    add use case

---
+++ delivery/lib/modules/restaurants/domain/usecase/get_restaurants_usecase.dart
@@ -0,0 +1,17 @@
+
+import 'package:delivery/modules/restaurants/domain/entities/restaurant.dart';
+
+abstract class GetRestaurantsUseCase {
+  Future<List<Restaurant>> call();
+}
+
+class GradesListUseCaseImpl implements GetRestaurantsUseCase {
+  @override
+  Future<List<Restaurant>> call() {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+
+
+  //Future<List<Restaurant>> call(); //async => gradesListRepository(pageNumber);
+}
