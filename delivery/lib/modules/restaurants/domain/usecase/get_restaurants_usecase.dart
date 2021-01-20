4d283329cdca4856d41579ce0741472cce45daec
Alvaro Menezes <alvarom@ufcspa.edu.br>
2021-01-20 18:48:24 -0300

    get restaurants use case tested

--- delivery/lib/modules/restaurants/domain/usecase/get_restaurants_usecase.dart
+++ delivery/lib/modules/restaurants/domain/usecase/get_restaurants_usecase.dart
@@ -1,17 +1,15 @@
-
 import 'package:delivery/modules/restaurants/domain/entities/restaurant.dart';
+import 'package:delivery/modules/restaurants/domain/repositories/get_restaurants_repositories.dart';
 
 abstract class GetRestaurantsUseCase {
   Future<List<Restaurant>> call();
 }
 
 class GetRestaurantsUseCaseImpl implements GetRestaurantsUseCase {
+  final GetRestaurantsRepository repository;
+
+  GetRestaurantsUseCaseImpl(this.repository);
+
   @override
-  Future<List<Restaurant>> call() {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
-
-
-  //Future<List<Restaurant>> call(); //async => gradesListRepository(pageNumber);
+  Future<List<Restaurant>> call() async => repository();
 }
--- delivery/test/modules/restaurants/domain/usecase/get_restaurants_usecase_test.dart
+++ delivery/test/modules/restaurants/domain/usecase/get_restaurants_usecase_test.dart
@@ -1,24 +1,29 @@
-
 import 'package:delivery/modules/restaurants/domain/entities/restaurant.dart';
+import 'package:delivery/modules/restaurants/domain/repositories/get_restaurants_repositories.dart';
 import 'package:delivery/modules/restaurants/domain/usecase/get_restaurants_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class RepositoryMock extends Mock implements GetRestaurantsRepository {}
 
 main() {
   GetRestaurantsUseCase useCase;
-
+  GetRestaurantsRepository repository;
 
   setUp(() async {
-
-    useCase = GetRestaurantsUseCaseImpl();
+    repository = RepositoryMock();
+    useCase = GetRestaurantsUseCaseImpl(repository);
   });
 
   group('Get Restaurants Use Case', () {
-
     test('Must return list of restaurants ', () async {
+      List<Restaurant> restaurants = [];
+      restaurants.add(Restaurant("", "", "", "", "", "", "", "", "", ""));
+
+      when(repository()).thenAnswer((_) async => restaurants);
 
       var response = await useCase();
       expect(response, isA<List<Restaurant>>());
-
     });
 
 /*
